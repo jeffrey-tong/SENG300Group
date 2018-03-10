c28ed9af5e8ace1a27926dc7762aa1f59746b921
jr12301 <wilfredojp3@gmail.com>
2018-03-10 13:22:27 -0700

    Creation of Version 2 of group project. Need to fix bug when setting unit name or setting environment.

---
+++ SENG300Group/src/parser/Test.java
@@ -0,0 +1,20 @@
+package parser;
+
+public class Test {
+
+	private String test;
+	private String test2;
+	private String test3; 
+	private String test4;
+	private int testInt; 
+	private String test5; 
+	
+	public static void main(String args[]) {
+		test = "test"; 
+		test = "test"; 
+		test = "test"; 
+		test = "test"; 
+		test = "test"; 
+		testInt = 9; 
+	}
+}
